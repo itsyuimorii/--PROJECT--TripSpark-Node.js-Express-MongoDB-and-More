0145a22254d97cc2b7014a55ca45da32d277dda4
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-05-04 13:53:03 -0500

    Update appError.js

--- utils/appError.js
+++ utils/appError.js
@@ -1,11 +1,18 @@
 class AppError extends Error {
   constructor(message, statusCode) {
+    // call the parent constructor and pass the error message
     super(message);
 
+    // set the HTTP status code
     this.statusCode = statusCode;
+
+    // set the status of the error
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+
+    // set the isOperational flag to true
     this.isOperational = true;
 
+    // capture the stack trace of the error
     Error.captureStackTrace(this, this.constructor);
   }
 }
