532d2c7c7b408a8bb54e32921b5937ca92354b41
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-05-16 20:20:39 -0500

    clear commit history

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+router.patch(
+  '/updateMyPassword',
+  authController.protect,
+  authController.updatePassword
+);
+
+router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
 router
   .route('/')
