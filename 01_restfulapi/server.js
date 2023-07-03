b649186bbc5082e6950e8b7aabe90e4ac6ad3f60
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-07-02 22:14:40 -0500

    clear commit history

--- 01_restfulapi/server.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-const app = require('./app');
-
-dotenv.config({ path: './config.env' });
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
