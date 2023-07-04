f52536dea9d0611a0933bd9349da7bd3fe30abc8
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-07-03 19:38:00 -0500

    restful api

--- app.js
+++ app.js
@@ -21,23 +21,48 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.post('api/v1/tours', (req, res) => {
+app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   // Object.assign() method is used to copy the values of all enumerable own properties from one or more source objects to a target object. It will return the target object.
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    res.status(201).json({
-      status: 'success',
-    
-      data: {
-        tour: newTour
-      }
+  // write the new tours array to the file
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
     });
-    res.send('Done');
+  // res.send('Done');
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  
+  const tour = tours.find(ele => ele.id === id)
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
   });
 });
+
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
