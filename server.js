f6d5c5f46d1194a0b8862e72e6e2fa3f7ca4f6f0
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-07-07 17:56:56 -0500

    mongo connection

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const dotenv = require('dotenv');
-const app = require('./app');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
-console.log(process.env);
+const app = require('./app');
+
+// console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}).then(con => {
+  console.log(con.connections);
+  console.log('DB connection successful!');
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
