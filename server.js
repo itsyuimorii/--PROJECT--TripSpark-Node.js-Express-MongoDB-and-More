242d20d00f55970eda786c492c343420995f2692
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-07-07 19:39:18 -0500

    tourSchema

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv');
+const validator = require('validator');
 const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
@@ -23,6 +24,24 @@ mongoose.connect(DB, {
   console.log('DB connection successful!');
 });
 
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'], //validator
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
