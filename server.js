aebe083e47aaa12884dcf66a21851f66263c6c80
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-07-16 16:46:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose
   })
   .then(() => console.log('DB connection successful!'));
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
