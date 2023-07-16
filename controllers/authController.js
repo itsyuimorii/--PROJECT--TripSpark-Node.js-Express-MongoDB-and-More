272fb175e0325af6bfaee34fa69f35eec0d174df
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-07-16 11:39:48 -0500

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -63,35 +63,47 @@ exports.login = catchAsync(async(req, res, next) => {
 exports.protect = catchAsync(async (req, res, next) => {
     // 1) Getting token and check of it's there
     let token;
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-        token = req.headers.authorization.split(' ')[1];
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
     }
+  
     // console.log(token);
-    if(!token) {
-        return next(new AppError('💥You are not logged in! Please log in to get access.', 401));
+    if (!token) {
+      return next(
+        new AppError('You are not logged in! Please log in to get access.', 401)
+      );
     }
     //  authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YjBiYmQ5Zjg3NzgyZjhhYzg5OTI3NyIsImlhdCI6MTY4OTMwNDAyNSwiZXhwIjoxNjk3MDgwMDI1fQ.Ic45IuSdWdMfXmFT701iTQDlFpplQcg2wHHMceqx39s',
 
     // 2) Verification token
     const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
     console.log(decoded);
     //{ id: '64b0bbd9f87782f8ac899277', iat: 1689304025, exp: 1697080025 }
  
-    // 3) Check if user still exists
-    const freshUser = await User.findById(decoded.id);
+    
+  // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError(
+        'The user belonging to this token does no longer exist.',
+        401
+      )
+    );
+  }
 
-    user.findOne({ _id: decoded.id });
-    //if user is not found or deleted
-    if(!currentUser) {
-        return next(new AppError('💥The user belonging to this token do es no longer exist.', 401));
-    } 
-    // 4) Check if user changed password after the token was issued
-    if(currentUser.changedPasswordAfter(decoded.iat)) {
-        return next(new AppError('💥User recently changed password! Please log in again.', 401));
-    }
+  // 4) Check if user changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please log in again.', 401)
+    );
+  }
   // GRANT ACCESS TO PROTECTED ROUTE
-    req.user = currentUser;
-    next();
+  req.user = currentUser;
+  next();
 });
-
  
