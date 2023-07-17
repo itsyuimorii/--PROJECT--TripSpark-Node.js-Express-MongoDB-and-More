e32b7e1a78d762f53e947b766bb43f84fa04992f
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-07-16 20:57:54 -0500

    Update email.js

--- utils/email.js
+++ utils/email.js
@@ -16,7 +16,7 @@ const sendEmail = async options => {
 
   // 2) Define the email options
   const mailOptions = {
-    from: 'Jonas Schmedtmann <hello@jonas.io>',
+    from: 'yui morii <hello@yuimorii.io>',
     to: options.email,
     subject: options.subject,
     text: options.message
