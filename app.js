fdbe94ecab265957a70dffa932762aba07915a39
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-07-21 18:52:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,14 +9,27 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from the same IP address
+const limiter = rateLimit({
+  max: 100, // 100 requests per hour
+  windowMs: 60 * 60 * 1000, // 1 hour
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+
+// Apply the rate limiter middleware to all requests to the /api route
+app.use('/api', limiter);
+
+// Body parser, reading data from the body into req.body
 app.use(express.json());
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   console.log(req.headers);
