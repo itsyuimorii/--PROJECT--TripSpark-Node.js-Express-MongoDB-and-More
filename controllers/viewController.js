21c49ed08a962bbfbc726c633a4f298627b0c34b
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-08-14 16:31:58 -0500

    pug template

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,17 +1,23 @@
-exports.getOverview = (req, res) => {
+const Tour = require('./../models/tourModel');
+const catchAsync = require('./../utils/catchAsync');
+
+
+exports.getOverview = catchAsync (async(req, res, next) => {
     // 1）Get tour data from collection
-    
+    const tours = await Tour.find();
+
     // 2）Build template
     // 3）Render that template using tour data from 1)
 
     res.status(200).render('overview',{
-        title: 'All Tours'
+        title: 'ALL TOURS',
+        tours // tours: tours
     });
-}
+});
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync (async(req, res, next) => {
     res.status(200).render('base',{
         tour:'The Forest Hiker',
-        user:'Yuimorii'
+        user:'itsyuimorii'
     });
-}
+});
