cac9609996e8de3b1d7f66dc4f6c98fb16bb0b72
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-08-18 15:59:46 -0500

    update

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -2,22 +2,32 @@ const Tour = require('./../models/tourModel');
 const catchAsync = require('./../utils/catchAsync');
 
 
-exports.getOverview = catchAsync (async(req, res, next) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
     // 1）Get tour data from collection
     const tours = await Tour.find();
 
     // 2）Build template
     // 3）Render that template using tour data from 1)
 
-    res.status(200).render('overview',{
+    res.status(200).render('overview', {
         title: 'ALL TOURS',
         tours // tours: tours
     });
 });
 
-exports.getTour = catchAsync (async(req, res, next) => {
-    res.status(200).render('base',{
-        tour:'The Forest Hiker',
-        user:'itsyuimorii'
+exports.getTour = catchAsync(async (req, res, next) => {
+    // 1) Get the data, for the requested tour (including reviews and guides)
+    const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+        path: 'reviews',
+        fields: 'review rating user'
+    });
+
+    // 2) Build template
+    // 3) Render template using data from 1)
+    res.status(200).render('tour', {
+        title: `${tour.name} Tour`,
+        tour
     });
 });
+
+
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const viewController = require('../controllers/viewController');
 
-const router= express.Router();
+const router = express.Router();
 
 router.get('/', viewController.getOverview);
-router.get('/tour', viewController.getTour);
+router.get('/tour/:slug', viewController.getTour);
 
 
 
