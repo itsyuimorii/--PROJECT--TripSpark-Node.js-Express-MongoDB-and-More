f3d67bbe874b1ba077f679b6b9811c1f9ad03069
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-08-19 11:11:44 -0500

    update css

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -25,9 +25,7 @@ exports.getTour = catchAsync(async (req, res, next) => {
     // 2) Build template
     // 3) Render template using data from 1)
     res.status(200).render('tour', {
-        title: `${tour.name} Tour`,
-        tour
+        title: "the sea explorer tour",
+        tour: tour
     });
 });
-
-
