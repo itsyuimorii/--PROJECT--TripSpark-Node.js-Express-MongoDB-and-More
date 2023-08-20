e0e930d31d02f46773f699a113f757f6b4a06596
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-08-20 17:03:13 -0500

    login logic

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,21 @@
+import axios from 'axios';
+
 const login = async (email, password) => {
+    console.log(email, password)
     alert(email + ' ' + password);
+    try {
+        const res = await axios({
+            method: 'POST',
+            url: 'http://127.0.0.1:3000/api/v1/users/login',
+            data: {
+                email,
+                password
+            }
+        });
+        console.log(res);
+    } catch (error) {
+        console.error('Error:', error);
+    }
 };
 
 document.querySelector('.form').addEventListener('submit', async e => {
