e48f82cc66f60268c13a75ec675f0b699cca3f42
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-08-20 10:58:13 -0500

    mapbox

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,3 +1,14 @@
-console.log('hello from the client side!') // this will be shown in the browser console
 
 const locations = JSON.parse(document.getElementById('map').dataset.locations)
+
+console.log(locations)
+
+mapboxgl.accessToken = 'pk.eyJ1IjoieXVpbW9yaWkiLCJhIjoiY2xsamxsZXd0MTQwejNkcjYwbzFscTYzOSJ9.x-BM1dPOqU1AR2jpuqJR7A';
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
+
