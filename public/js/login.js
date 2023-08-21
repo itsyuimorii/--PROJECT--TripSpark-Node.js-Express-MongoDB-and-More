c62d72e7c86154bcdd5a522693af7b30cbdf0dac
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-08-21 16:52:16 -0500

    mistake

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,10 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
 
-//----------------**LOGIN**----------------
-const login = async (email, password) => {
-    // console.log(email, password)
-    // alert(email + ' ' + password);
+export const login = async (email, password) => {
+    console.log('LOGIN')
+    console.log(email, password)
     try {
         const res = await axios({
             method: 'POST',
@@ -14,36 +16,25 @@ const login = async (email, password) => {
         });
 
         if (res.data.status === 'success') {
-            alert('Logged in successfully!');
+            showAlert('success', 'Logged in successfully!');
             window.setTimeout(() => {
                 location.assign('/');
             }, 1500);
         }
-
-
-        console.log(res);
-    } catch (error) {
-        console.error('Error:', error.response.data.message);
+    } catch (err) {
+        showAlert('error', err.response.data.message);
     }
 };
 
-//----------------**LOGOUT**----------------
 export const logout = async () => {
     try {
         const res = await axios({
             method: 'GET',
             url: 'http://127.0.0.1:3000/api/v1/users/logout'
         });
-        if (res.data.status === 'success') location.reload(true);
-    }
-    catch (error) {
-        console.error('Error:', error.response);
+        if ((res.data.status = 'success')) location.reload(true);
+    } catch (err) {
+        console.log(err.response);
+        showAlert('error', 'Error logging out! Try again.');
     }
 };
-
-document.querySelector('.form').addEventListener('submit', async e => {
-    e.preventDefault();
-    const email = document.querySelector('#email').value;
-    const password = document.querySelector('#password').value;
-    await login(email, password);
-});
