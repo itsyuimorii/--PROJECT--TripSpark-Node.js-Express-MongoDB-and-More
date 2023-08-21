fdcbfafb4b63567c469879f9b0d6ce678a1a3f28
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-08-20 21:43:33 -0500

    logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -81,6 +81,16 @@ exports.signup = catchAsync(async (req, res, next) => {
   createSendToken(newUser, 201, res);
 });
 
+//--------------**LOGOUT USER**----------------
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 1000), //expires in 10 seconds
+    httpOnly: true
+  });
+  res.status(200).json({ status: 'success' });
+};
+
+
 //--------------**LOGIN USER**----------------
 exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
@@ -273,30 +283,27 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
 // Only for rendered pages, no errors!
 exports.isLoggedIn = async (req, res, next) => {
   if (req.cookies.jwt) {
-    try {
-      // 1) verify token
-      const decoded = await promisify(jwt.verify)(
-        req.cookies.jwt,
-        process.env.JWT_SECRET
-      );
 
-      // 2) Check if user still exists
-      const currentUser = await User.findById(decoded.id);
-      if (!currentUser) {
-        return next();
-      }
+    // 1) verify token
+    const decoded = await promisify(jwt.verify)(
+      req.cookies.jwt,
+      process.env.JWT_SECRET
+    );
 
-      // 3) Check if user changed password after the token was issued
-      if (currentUser.changedPasswordAfter(decoded.iat)) {
-        return next();
-      }
-
-      // THERE IS A LOGGED IN USER
-      res.locals.user = currentUser;
+    // 2) Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
       return next();
-    } catch (err) {
-      next();
     }
+
+    // 3) Check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      return next();
+    }
+
+    // THERE IS A LOGGED IN USER
+    res.locals.user = currentUser;
+    return next();
   }
   next();
 };
--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,5 @@
 
-
+//----------------**LOGIN**----------------
 const login = async (email, password) => {
     // console.log(email, password)
     // alert(email + ' ' + password);
@@ -12,9 +12,32 @@ const login = async (email, password) => {
                 password
             }
         });
+
+        if (res.data.status === 'success') {
+            alert('Logged in successfully!');
+            window.setTimeout(() => {
+                location.assign('/');
+            }, 1500);
+        }
+
+
         console.log(res);
     } catch (error) {
         console.error('Error:', error.response.data.message);
+    }
+};
+
+//----------------**LOGOUT**----------------
+export const logout = async () => {
+    try {
+        const res = await axios({
+            method: 'GET',
+            url: 'http://127.0.0.1:3000/api/v1/users/logout'
+        });
+        if (res.data.status === 'success') location.reload(true);
+    }
+    catch (error) {
+        console.error('Error:', error.response);
     }
 };
 
