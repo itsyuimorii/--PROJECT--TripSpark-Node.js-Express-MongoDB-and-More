60f4957028f8c861cfb3426e6586c5b5e521eb8c
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-09-02 23:20:04 -0500

    Update email.js

--- utils/email.js
+++ utils/email.js
@@ -12,7 +12,7 @@ module.exports = class Email {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = `yuimorii <${process.env.EMAIL_FROM}>`;
+    this.from = `yui morii<${process.env.EMAIL_FROM}>`;
   }
 
   newTransport() {
