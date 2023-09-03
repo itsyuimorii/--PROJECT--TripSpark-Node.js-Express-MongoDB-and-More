39dc9eceb0a6cb6b4eec724935ba961ece7deb20
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-09-02 22:48:35 -0500

    user photo

--- controllers/userController.js
+++ controllers/userController.js
@@ -83,6 +83,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   // 2) Filtered out unwanted fields names that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
 
   // 3) Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
