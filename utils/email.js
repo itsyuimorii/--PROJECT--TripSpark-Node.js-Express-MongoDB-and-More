4756c5377fb1bfb83f4bbbdd34d87b48743be83c
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-09-02 23:21:29 -0500

    Update email.js

--- utils/email.js
+++ utils/email.js
@@ -18,13 +18,7 @@ module.exports = class Email {
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
       // Sendgrid
-      return nodemailer.createTransport({
-        service: 'SendGrid',
-        auth: {
-          user: process.env.SENDGRID_USERNAME,
-          pass: process.env.SENDGRID_PASSWORD
-        }
-      });
+
     }
 
     return nodemailer.createTransport({
