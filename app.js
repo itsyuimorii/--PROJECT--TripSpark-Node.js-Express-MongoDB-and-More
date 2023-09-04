46013260079a96584edf26c89df1d6dbaaf9894f
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-09-03 23:43:37 -0500

    booking model

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
+
 
 
 const app = express();
@@ -23,36 +25,27 @@ app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
 // Serving static files
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Set security HTTP headers
 app.use(helmet());
-
-//fixing the mapbox security error
-app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "script-src 'self' https://api.tiles.mapbox.com https://cdn.jsdelivr.net; worker-src 'self' blob:;");
-  next();
-});
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// Limit requests from the same IP address
+// Limit requests from same API
 const limiter = rateLimit({
-  max: 100, // 100 requests per hour
-  windowMs: 60 * 60 * 1000, // 1 hour
+  max: 100,
+  windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!'
 });
-
-// Apply the rate limiter middleware to all requests to the /api route
 app.use('/api', limiter);
 
-// Body parser, reading data from the body into req.body
-app.use(express.json({
-  limit: '10kb'
-}));
-
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
@@ -75,28 +68,24 @@ app.use(
   })
 );
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
-
-
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.cookies);
+  // console.log(req.cookies);
   next();
 });
 
+// 3) ROUTES
 app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
-// 4) ROUTE HANDLERS
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-// 5) ERROR HANDLING MIDDLEWARE
 app.use(globalErrorHandler);
 
 module.exports = app;
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,15 +1,27 @@
 const express = require('express');
-const viewController = require('../controllers/viewController');
+const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.use(authController.isLoggedIn)
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
 
-router.get('/', viewController.getOverview);
-router.get('/tour/:slug', authController.protect, viewController.getTour);
+router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
+router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
+router.get('/me', authController.protect, viewsController.getAccount);
 
-router.get('/login', viewController.getLoginForm)
-router.get('/me', viewController.getAccount);
+// router.get(
+//     '/my-tours',
+//     bookingController.createBookingCheckout,
+//     authController.protect,
+//     viewsController.getMyTours
+// );
+
+router.post(
+    '/submit-user-data',
+    authController.protect,
+    viewsController.updateUserData
+);
 
 module.exports = router;
