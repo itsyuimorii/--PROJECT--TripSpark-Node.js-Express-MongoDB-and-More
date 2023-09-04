5d2b234e92cc2398ce99c5d4f33ec146db7a247e
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-09-04 18:36:21 -0500

    imageupload

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -5747,7 +5747,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54740" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60183" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,16 +8,23 @@
  * @version 1.0.0
  */
 const express = require('express');
+const multer = require('multer');
 const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
 
 const router = express.Router();
+const upload = multer({ dest: 'public/img/users' })
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
+
+
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
+
+
+
 
 // Protect all routes after this middleware
 router.use(authController.protect);
