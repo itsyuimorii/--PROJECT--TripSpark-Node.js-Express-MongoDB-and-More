de0627246fefc52b5a7d994201704d33721b8d26
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-09-05 14:54:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 
 
 const app = express();
@@ -86,10 +87,14 @@ app.use((req, res, next) => {
   next();
 });
 
+
+
+// 3) ROUTES
 app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 // 4) ROUTE HANDLERS
 app.all('*', (req, res, next) => {
