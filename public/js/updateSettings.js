47c1caa34163aa8e115a5f59fb042b7d831dc738
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-09-05 20:39:27 -0500

    preparing deploy

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -9,8 +9,8 @@ export const updateSettings = async (data, type) => {
 
     const url =
       type === 'password'
-        ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword'
-        : 'http://127.0.0.1:3000/api/v1/users/updateMe';
+        ? '/api/v1/users/updateMyPassword'
+        : '/api/v1/users/updateMe';
 
 
     const res = await axios({
