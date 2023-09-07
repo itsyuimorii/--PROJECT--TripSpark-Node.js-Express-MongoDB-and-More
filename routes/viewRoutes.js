b8b620a1ccbaa28fd9359824a96031ce2f6bc71d
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-09-06 22:43:51 -0500

    Update viewRoutes.js

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -6,7 +6,8 @@ const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-// router.use(authController.isLoggedIn)
+router.use(authController.isLoggedIn)
+
 router.get('/',
     bookingController.createBookingCheckout,
     authController.isLoggedIn,
